bd8f0a604bb664fa78f62f04a48c4c855c68d556
Alon Regev <alonr3704@gmail.com>
2021-04-16 22:23:00 +0300

    implemented serialization of structs

--- trivia/trivia/JsonResponsePacketSerializer.cpp
+++ trivia/trivia/JsonResponsePacketSerializer.cpp
@@ -1,1 +1,47 @@
 #include "JsonResponsePacketSerializer.h"
+
+Buffer JsonResponsePacketSerializer::serializeResponse(LoginResponse response)
+{
+	json content = {
+		{"status", response.status}
+	};
+	return createResponseBuffer(1, content);
+}
+
+Buffer JsonResponsePacketSerializer::serializeResponse(SignupResponse response)
+{
+	json content = {
+		{"status", response.status}
+	};
+	return createResponseBuffer(2, content);
+}
+
+Buffer JsonResponsePacketSerializer::serializeResponse(ErrorResponse response)
+{
+	json content = {
+		{"message", response.message}
+	};
+	return createResponseBuffer(0, content);
+}
+
+Buffer JsonResponsePacketSerializer::createResponseBuffer(Byte code, json& content)
+{
+	std::string jsonDump = content.dump();
+	// get content length as bytes
+	int contentLengthInt = jsonDump.size();
+	Byte* contentLengthBytes = (Byte*)&contentLengthInt;
+	// create buffer
+	Buffer buffer;
+	buffer.push_back(code);
+	buffer.insert(buffer.end(), contentLengthBytes, contentLengthBytes + 4);
+	buffer.insert(buffer.end(), jsonDump.begin(), jsonDump.end());
+
+	for (const Byte& b : buffer)
+		std::cout << (int)b << " ";
+	std::cout << std::endl;
+	for (const Byte& b : buffer)
+		std::cout << b;
+	std::cout << std::endl << std::endl;
+
+	return buffer;
+}
--- trivia/trivia/JsonResponsePacketSerializer.h
+++ trivia/trivia/JsonResponsePacketSerializer.h
@@ -10,5 +10,8 @@ public:
 	static Buffer serializeResponse(LoginResponse);
 	static Buffer serializeResponse(SignupResponse);
 	static Buffer serializeResponse(ErrorResponse);
+private:
+	// function gets response code and json and returns a buffer that can be sent back to the client
+	static Buffer createResponseBuffer(Byte code, json& content);
 };
 
