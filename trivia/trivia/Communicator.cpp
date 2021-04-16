47ff5dac417a8d02fbb4531aac670ed304749db9
Alon Regev <alonr3704@gmail.com>
2021-04-16 21:19:31 +0300

    fixed socket close after CR

--- trivia/trivia/Communicator.cpp
+++ trivia/trivia/Communicator.cpp
@@ -77,6 +77,7 @@ void Communicator::deleteClient(SOCKET clientSocket)
 	std::lock_guard<std::mutex> clientMapLock(this->m_clientMapMutex);
 	delete this->m_clients[clientSocket];
 	this->m_clients.erase(clientSocket);
+	closesocket(clientSocket);
 }
 
 Communicator::~Communicator()
