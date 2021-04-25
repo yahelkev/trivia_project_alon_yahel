9799f7296ec9ee6bbb7c35c3fc4aff87f31bfb1f
Alon Regev <alonr3704@gmail.com>
2021-04-25 22:51:10 +0300

    implemented sqlite database connection

--- trivia/trivia/SqliteDatabase.cpp
+++ trivia/trivia/SqliteDatabase.cpp
@@ -1,1 +1,52 @@
 #include "SqliteDatabase.h"
+
+SqliteDatabase::SqliteDatabase(std::string dbPath)
+{
+    // check if db already exists
+    bool fileExists = _access(dbPath.c_str(), 0) != -1;
+    // open db
+    bool success = sqlite3_open(dbPath.c_str(), &this->_database) == SQLITE_OK;
+    if (!fileExists && success)
+    {   // new db, create tables
+        this->executeQuery(
+            "CREATE TABLE IF NOT EXISTS Users("
+            "id INTEGER PRIMARY KEY,"
+            "username TEXT NOT NULL,"
+            "password TEXT NOT NULL,"
+            "email TEXT"
+            ");"
+        );
+    }
+    if (!success)
+        throw std::exception("Opening Database Failed...");
+}
+
+SqliteDatabase::~SqliteDatabase()
+{
+    sqlite3_close(this->_database);
+}
+
+bool SqliteDatabase::executeQuery(const std::string& sql, callbackFunction callback, void* callbackData)
+{
+    char* errorMsg = nullptr;
+    bool empty = true;
+    void* data[3] = { callback, callbackData, &empty };
+    int res = sqlite3_exec(this->_database, sql.c_str(),
+        // callback
+        [](void* data, int argc, char** argv, char** cols)
+        {
+            // get data from param
+            callbackFunction callback = (callbackFunction)((void**)data)[0];
+            void* callbackData = ((void**)data)[1];
+            bool& empty = *(bool*)((void**)data)[2];
+            empty = false;
+            // call original callback
+            return callback(callbackData, argc, argv, cols);
+        },
+        data, &errorMsg);
+    if (res != SQLITE_OK)
+    {
+        throw std::exception(("SQL ERROR: " + std::string(errorMsg)).c_str());
+    }
+    return !empty;
+}
--- trivia/trivia/SqliteDatabase.h
+++ trivia/trivia/SqliteDatabase.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <io.h>
 #include "IDatabase.h"
 #include "sqlite3.h"
+
+typedef int(*callbackFunction)(void*, int, char**, char**);
 
 class SqliteDatabase : public IDatabase
 {
@@ -11,6 +14,9 @@ public:
 	virtual bool doesPasswordMatch(std::string username, std::string password) { return true; }
 	virtual bool addNewUser(std::string username, std::string password, std::string email) { return true; }
 private:
+	// function executes a sql query in the opened database. returns whether the callback was used or not.
+	bool executeQuery(const std::string& sql, callbackFunction callback=nullptr, void* callbackData=nullptr);
+
 	sqlite3* _database;
 };
 
