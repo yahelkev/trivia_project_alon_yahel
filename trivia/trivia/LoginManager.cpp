20f74c7cf943e3376ac99689b49fecd9fc164a5a
Yahel Kevelson <yahelkev@gmail.com>
2021-04-27 19:02:38 +0300

    merged with loggin manager manualy

--- trivia/trivia/LoginManager.cpp
+++ trivia/trivia/LoginManager.cpp
@@ -1,4 +1,5 @@
 #include "LoginManager.h"
+std::mutex m_loggedUsersMutex;
 
 bool LoginManager::signup(const std::string& username, const std::string& password, const std::string& email)
 {
@@ -11,6 +12,8 @@ bool LoginManager::signup(const std::string& username, const std::string& passwo
 
 bool LoginManager::login(const std::string& username, const std::string& password)
 {
+	//logs in only if the user exists and and it is not loged in yet
+	std::lock_guard<std::mutex> loggedUsersLock(m_loggedUsersMutex);
 	if (this->m_database->doesUserExist(username) && (std::find(m_loggedUsers.begin(), m_loggedUsers.end(), LoggedUser(username)) == m_loggedUsers.end()))
 	{
 		if (this->m_database->doesPasswordMatch(username, password))
@@ -24,6 +27,8 @@ bool LoginManager::login(const std::string& username, const std::string& passwor
 
 bool LoginManager::logout(const std::string& username)
 {
+	//logs out only if the user is loged in
+	std::lock_guard<std::mutex> loggedUsersLock(m_loggedUsersMutex);
 	if ((std::find(m_loggedUsers.begin(), m_loggedUsers.end(), LoggedUser(username)) != m_loggedUsers.end()))
 	{
 		m_loggedUsers.erase(std::remove(m_loggedUsers.begin(), m_loggedUsers.end(), LoggedUser(username)), m_loggedUsers.end());
--- trivia/trivia/LoginManager.h
+++ trivia/trivia/LoginManager.h
@@ -1,16 +1,19 @@
 #pragma once
+
+#include <algorithm>
 #include <vector>
+#include <mutex>
 #include "LoggedUser.h"
 #include "IDatabase.h"
 
 class LoginManager
 {
 public:
-	//LoginManager() = default;
-	LoginManager(IDatabase* database) : m_database(database) {};
+	LoginManager(IDatabase* dataBase) : m_database(dataBase) {};
 	bool signup(const std::string& username, const std::string& password, const std::string& email);
 	bool login(const std::string& username, const std::string& password);
 	bool logout(const std::string& username);
+
 private:
 	IDatabase* m_database;
 	std::vector<LoggedUser> m_loggedUsers;
