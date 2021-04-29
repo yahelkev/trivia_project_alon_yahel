e7b517fe7707f21170cd896149b2070de6770bd7
Yahel Kevelson <yahelkev@gmail.com>
2021-04-29 13:58:52 +0300

    done with Room Class

--- trivia/trivia/Room.cpp
+++ trivia/trivia/Room.cpp
@@ -1,1 +1,36 @@
 #include "Room.h"
+
+bool Room::addUser(LoggedUser newUser)
+{
+    std::lock_guard<std::mutex> usersLock(this->m_users_lock);
+    //add if not exists
+    if (std::find(m_users.begin(), m_users.end(), newUser) == m_users.end())
+    {
+        m_users.push_back(newUser);
+        return true;
+    }
+    return false;
+}
+
+bool Room::removeUser(LoggedUser user)
+{
+    //removes only if the user is in the room
+    std::lock_guard<std::mutex> usersLock(m_users_lock);
+    if ((std::find(m_users.begin(), m_users.end(), LoggedUser(user)) != m_users.end()))
+    {
+        m_users.erase(std::remove(m_users.begin(), m_users.end(), LoggedUser(user)), m_users.end());
+        return true;
+    }
+    return false;
+}
+
+std::vector<std::string> Room::getAllUsers()
+{
+    std::vector<std::string> result;
+    std::lock_guard<std::mutex> usersLock(m_users_lock);
+    for (auto i = m_users.begin(); i != m_users.end(); i++)
+    {
+        result.push_back(i->getUsername());
+    }
+    return result;
+}
--- trivia/trivia/Room.h
+++ trivia/trivia/Room.h
@@ -1,15 +1,20 @@
 #pragma once
 #include "MessageStructs.h"
 #include "LoggedUser.h"
+#include <mutex>
 
 class Room
 {
 	RoomData m_metadata;
 	std::vector<LoggedUser> m_users;
+	std::mutex m_users_lock;
 
 public:
+	//adds user to room
 	bool addUser(LoggedUser);
+	//removes user from room
 	bool removeUser(LoggedUser);
+	//gets all userss names in the room
 	std::vector<std::string> getAllUsers();
 };
 
