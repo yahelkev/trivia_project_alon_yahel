db8fbd1b6f8fe74ef978496a930944c1da4c9740
Alon Regev <alonr3704@gmail.com>
2021-05-01 14:15:12 +0300

    added statistics manager declaration

---
+++ trivia/trivia/StatisticsManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "IDatabase.h"
+#include "UserStatistics.h"
+
+class StatisticsManager
+{
+public:
+	std::list<UserStatistics> getStatistics();
+
+	static float scoreFormula(float averageAnswerTime, int correctAnswers, int totalAnswers, int gameCount);
+private:
+	IDatabase* m_database;
+
+	std::list<UserStatistics> getHighScores();
+	UserStatistics getUserStatistics(const std::string& username);
+};
+
