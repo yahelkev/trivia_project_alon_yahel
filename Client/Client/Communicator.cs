f80ffe3dd2a77e1868685fbd066ec99c462e8150
Yahel Kevelson <yahelkev@gmail.com>
2021-05-08 22:36:46 +0300

    done

--- Client/Client/Communicator.cs
+++ Client/Client/Communicator.cs
@@ -5,45 +5,116 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
+using System.Net.Sockets;
+using System.Net;
+
 namespace Client
 {
 	public class Communicator
 	{
+		private const int PORT = 6666;
+		private const int REQUEST_CODE_BYTES = 1;
+		private const int CONTENT_LENGTH_BYTES = 4;
+		private const string SERVER_IP = "127.0.0.1";
+		private NetworkStream _clientStream;
+		public Communicator()
+		{
+			TcpClient client = new TcpClient();
+			IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(SERVER_IP), PORT);
+			client.Connect(serverEndPoint);
+			_clientStream = client.GetStream();
+		}
+
+		enum MSG_CODES
+		{
+			ERROR_CODE = 0,
+			LOGIN,
+			SIGNUP,
+			LOGOUT,
+			GET_ROOMS,
+			GET_PLAYERS_IN_ROOM,
+			JOIN_ROOM,
+			CREATE_ROOM,
+			HIGH_SCORE,
+			USER_STATISTICS
+		}
+		private byte[] sendMsg(int code, byte[] msg)
+		{
+			byte[] buffer = new byte[REQUEST_CODE_BYTES + CONTENT_LENGTH_BYTES + msg.Length];
+			buffer[0] = (byte)code;
+			BitConverter.GetBytes(msg.Length).CopyTo(buffer, REQUEST_CODE_BYTES);
+			msg.CopyTo(buffer, REQUEST_CODE_BYTES + CONTENT_LENGTH_BYTES);
+			_clientStream.Write(buffer, 0, buffer.Length);
+			_clientStream.Flush();
+			byte[] msgCode = new byte[REQUEST_CODE_BYTES];
+			byte[] contentLength = new byte[CONTENT_LENGTH_BYTES];
+			_clientStream.Read(msgCode, 0, REQUEST_CODE_BYTES);
+			_clientStream.Read(contentLength, 0, CONTENT_LENGTH_BYTES);
+			buffer = new byte[BitConverter.ToInt32(contentLength, 0)];
+			_clientStream.Read(buffer, 0, BitConverter.ToInt32(contentLength, 0));
+			if (msgCode[0] == (int)MSG_CODES.ERROR_CODE)
+			{
+
+				throw new Exception(Deserializer.deserializeResponse<ErrorResponse>(buffer).message);
+			}
+			return buffer;
+		}
 		public LoginResponse login(string username, string password)
 		{
-			return new LoginResponse();
+			LoginRequest msgData = new LoginRequest();
+			msgData.username = username;
+			msgData.password = password;
+			byte[] json = Serializer.serializeRequest<LoginRequest>(msgData);
+			return Deserializer.deserializeResponse<LoginResponse>(sendMsg((int)MSG_CODES.LOGIN, json));
 		}
 		public SignupResponse signup(string username, string password, string email)
 		{
-			return new SignupResponse();
+			SignupRequest msgData = new SignupRequest();
+			msgData.username = username;
+			msgData.password = password;
+			msgData.email = email;
+			byte[] json = Serializer.serializeRequest<SignupRequest>(msgData);
+			return Deserializer.deserializeResponse<SignupResponse>(sendMsg((int)MSG_CODES.SIGNUP, json));
 		}
 		public LogoutResponse logout()
 		{
-			return new LogoutResponse();
+			return Deserializer.deserializeResponse<LogoutResponse>(sendMsg((int)MSG_CODES.LOGOUT, new byte[] { }));
 		}
 		public JoinRoomResponse joinRoom(uint id)
 		{
-			return new JoinRoomResponse();
+			JoinRoomRequest msgData = new JoinRoomRequest();
+			msgData.roomID = id;
+			byte[] json = Serializer.serializeRequest<JoinRoomRequest>(msgData);
+			return Deserializer.deserializeResponse<JoinRoomResponse>(sendMsg((int)MSG_CODES.JOIN_ROOM, json));
 		}
 		public GetPlayersInRoomResponse getPlayersInRoom(uint id)
 		{
-			return new GetPlayersInRoomResponse();
+			GetPlayersInRoomRequest msgData = new GetPlayersInRoomRequest();
+			msgData.roomID = id;
+			byte[] json = Serializer.serializeRequest<GetPlayersInRoomRequest>(msgData);
+			return Deserializer.deserializeResponse<GetPlayersInRoomResponse>(sendMsg((int)MSG_CODES.GET_PLAYERS_IN_ROOM, json));
 		}
 		public CreateRoomResponse createRoom(string name, uint maxUsers, uint questionCount, uint answerTimeout)
 		{
-			return new CreateRoomResponse();
+			CreateRoomRequest msgData = new CreateRoomRequest();
+			msgData.roomName = name;
+			msgData.maxUsers = maxUsers;
+			msgData.questionCount = questionCount;
+			msgData.questionTimeout = answerTimeout;
+			byte[] json = Serializer.serializeRequest<CreateRoomRequest>(msgData);
+			return Deserializer.deserializeResponse<CreateRoomResponse>(sendMsg((int)MSG_CODES.CREATE_ROOM, json));
 		}
 		public GetPersonalStatisticsResponse getUserStatistics()
 		{
-			return new GetPersonalStatisticsResponse();
+			return Deserializer.deserializeResponse<GetPersonalStatisticsResponse>(sendMsg((int)MSG_CODES.USER_STATISTICS, new byte[] { }));
 		}
 		public GetHighScoresResponse getHighScores()
 		{
-			return new GetHighScoresResponse();
+			return Deserializer.deserializeResponse<GetHighScoresResponse>(sendMsg((int)MSG_CODES.HIGH_SCORE, new byte[] { }));
 		}
 		public GetRoomsResponse getRooms()
 		{
-			return new GetRoomsResponse();
+			return Deserializer.deserializeResponse<GetRoomsResponse>(sendMsg((int)MSG_CODES.GET_ROOMS, new byte[] { }));
 		}
 	}
 }
--- Client/Client/signupWindow.xaml.cs
+++ Client/Client/signupWindow.xaml.cs
@@ -12,24 +12,46 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using System.ComponentModel;
+
+
 namespace Client
 {
     public partial class signupWindow : Window
     {
         private Communicator _communicator;
+        private BackgroundWorker _worker = new BackgroundWorker();
         public signupWindow(Communicator communicator)
         {
             InitializeComponent();
             _communicator = communicator;
+            _worker.DoWork += do_work;
+            _worker.RunWorkerCompleted += work_complete;
+        }
+        private void do_work(object sender, DoWorkEventArgs e)
+        {
+            SignupResponse response = _communicator.signup(((string[])e.Argument)[0], ((string[])e.Argument)[1], ((string[])e.Argument)[2]);
+            e.Result = response.status;
+        }
+        private void work_complete(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+                return;
+            }
+            if ((uint)e.Result == 1)
+            {
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("invalid input!");
+            }
         }
         private void signup(object sender, RoutedEventArgs e)
         {
-            SignupResponse response = _communicator.signup(inputUserName.Text, inputPassword.Text, inputemail.Text);
-            if (response.status == 0)
-            {
-                MessageBox.Show("Something went wrong");
-            }
-            this.Close();
+            _worker.RunWorkerAsync(new string[] { inputUserName.Text, inputPassword.Text, inputEmail.Text });
         }
         private void cancel(object sender, RoutedEventArgs e)
         {
